cf00da8d02bae0a02cd0b332b93df35a88931857
Kurt Wolfer <kwolfer2@gmail.com>
2025-04-04 18:42:07 +0000

    feat: continued work on number_guess.sh, adding GUESS_MENU(){}

--- number_guess.sh
+++ number_guess.sh
@@ -19,3 +19,18 @@ BEST_GAME=$($PSQL "SELECT best_guess FROM users WHERE username='$RETURNING_USER'
 echo "Welcome back, $RETURNING_USER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
+GUESS_MENU() {
+echo -e "Guess the number between 1 and 1000:"
+read CURRENT_GUESS
+
+if [[ $CURRENT_GUESS -eq $SECRET_NUMBER ]]
+then echo "YOU WIN!"
+$PSQL "INSERT INTO users(games_played) VALUES ($GAMES_PLAYED + 1) WHERE username = '$INPUT_USERNAME'"
+elif [[ $CURRENT_GUESS -gt $SECRET_NUMBER ]]
+then
+GUESS_MENU "It's lower than that, guess again:"
+elif [[ $CURRENT_GUESS -lt $SECRET_NUMBER ]]
+then GUESS_MENU "It's higher than that, guess again:"
+fi
+}
+GUESS_MENU
